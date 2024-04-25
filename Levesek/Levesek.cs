f8fe00971e149e39322ac8a1accf48acfc083296
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2024-04-25 10:33:39 +0200

    Add files via upload

---
+++ Levesek/Levesek.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levesek
+{
+   
+    internal class Levesek
+    { 
+        public string megnevezes;
+        public double kaloria;
+        public double feherje;
+        public double zsir;
+        public double szenhidrat;
+        public double hamu;
+        public double rost;
+
+        public Levesek(string megnevezes, double kaloria, double feherje, double zsir, double szenhidrat, double hamu, double rost)
+        {
+            this.megnevezes = megnevezes;
+            this.kaloria = kaloria;
+            this.feherje = feherje;
+            this.zsir = zsir;
+            this.szenhidrat = szenhidrat;
+            this.hamu = hamu;
+            this.rost = rost;
+        }
+    }
+}
---
+++ Levesek/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levesek
+{
+    internal class Program
+    {
+        static Adatbazis db = new Adatbazis();
+        static List<Levesek> levesek1;
+        static void Main(string[] args)
+        {
+            levesek1 = db.getAllleves();
+            feladat01();
+            feladat02();
+            Console.WriteLine("Program Vége");
+            Console.ReadLine();
+        }
+        private static void feladat01()
+        {
+            Console.WriteLine("*** 1. Feladat! ***");
+            Console.WriteLine($"\t Levesek száma : {levesek1.Count} db");
+
+        }
+        private static void feladat02()
+        {
+            Console.WriteLine("*** 2. Feladat! ***");
+            double maxKaloria = levesek1.Max(a => a.kaloria);
+            Levesek levesek = levesek1.Find(a => a.kaloria == maxKaloria);
+            Console.WriteLine($"\t Legtöbb kalóriát érő leves : {levesek.megnevezes}");
+        }
+    }
+}
